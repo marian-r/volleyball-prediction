a0472176ccf9ab96d2f9ab922b3e519efd612c69
marianr <grizly21@gmail.com>
2014-11-20 13:18:25 +0100

    Added reading of results

---
+++ results.R
@@ -0,0 +1,49 @@
+
+read.result = function(fileName) 
+{
+	filePath = paste("data/results/", fileName, ".txt", sep="")
+	read.table(filePath, sep="\t", header=TRUE)
+}
+
+read.all.results = function()
+{
+	resultFiles = c("fivb_world_cup_2011", "fivb_world_championship_2010",
+			"fivb_world_championship_2014", "fivb_world_league_2010",
+			"fivb_world_league_2011", "fivb_world_league_2012",
+			"fivb_world_league_2013", "fivb_world_league_2014")
+	
+	results = data.frame()
+	
+	for (fileName in resultFiles)
+	{
+		newResults = read.result(fileName)
+		results = rbind(results, newResults)
+	}
+
+	results
+}
+
+divideColumnsByTeams = function(results, columnName, asInt = TRUE)
+{
+	charVector = as.character(results[,columnName])
+	spl = strsplit(charVector , "-")
+	splitVector = unlist(spl)
+
+	team1 = splitVector[seq(from=1, to=length(splitVector), by=2)]
+	team2 = splitVector[seq(from=2, to=length(splitVector), by=2)]
+
+	columnName1 = paste(columnName, "_1", sep="")
+	columnName2 = paste(columnName, "_2", sep="")
+	
+	if (asInt)
+	{
+		results[,columnName1] = as.integer(team1)
+		results[,columnName2] = as.integer(team2)
+	}
+	else
+	{
+		results[,columnName1] = as.factor(team1)
+		results[,columnName2] = as.factor(team2)
+	}
+	results
+}
