2da6c689e0f0264e2cf8d7cf62b94b1888a67a89
marianr <grizly21@gmail.com>
2014-11-26 21:10:44 +0100

    Added more columns when reading results

--- results.R
+++ results.R
@@ -1,7 +1,7 @@
 
 source("date_time_helpers.R")
 
-read.result = function(fileName) 
+read.result = function(fileName)
 {
 	filePath = paste("data/results/", fileName, ".txt", sep="")
 	read.table(filePath, sep="\t", header=TRUE, stringsAsFactors=FALSE)
@@ -13,15 +13,15 @@ read.all.results = function()
 			"fivb_world_championship_2014", "fivb_world_league_2010",
 			"fivb_world_league_2011", "fivb_world_league_2012",
 			"fivb_world_league_2013", "fivb_world_league_2014")
-	
+
 	results = data.frame()
-	
+
 	for (fileName in resultFiles)
 	{
 		newResults = read.result(fileName)
 		results = rbind(results, newResults)
 	}
-	
+
 	results
 }
 
@@ -29,9 +29,13 @@ prepareResults = function(res)
 {
 	res = divideColumnsByTeams(res, "Teams", FALSE)
 	res = divideColumnsByTeams(res, "Result")
+	res = divideColumnsByTeams(res, "Points")
 	res = divideColumnsByTeams(res, "Set1")
 	res = divideColumnsByTeams(res, "Set2")
-	
+	res = divideColumnsByTeams(res, "Set3")
+	res = divideColumnsByTeams(res, "Set4")
+	res = divideColumnsByTeams(res, "Set5")
+
 	res[res$Result_1 > res$Result_2, "Winner"] = "1"
 	res[res$Result_1 < res$Result_2, "Winner"] = "2"
 	res$Winner = as.factor(res$Winner)
@@ -45,24 +49,31 @@ prepareResults = function(res)
 divideColumnsByTeams = function(results, columnName, asInt = TRUE)
 {
 	charVector = as.character(results[,columnName])
+  charVector[which(is.na(charVector))] = "NA-NA"
+  print(charVector)
 	spl = strsplit(charVector , "-")
 	splitVector = unlist(spl)
 
-	team1 = splitVector[seq(from=1, to=length(splitVector), by=2)]
-	team2 = splitVector[seq(from=2, to=length(splitVector), by=2)]
+  print(columnName)
+  print(splitVector)
+
+  team1 = splitVector[seq(from=1, to=length(splitVector), by=2)]
+  team2 = splitVector[seq(from=2, to=length(splitVector), by=2)]
 
 	columnName1 = paste(columnName, "_1", sep="")
 	columnName2 = paste(columnName, "_2", sep="")
-	
+
 	if (asInt)
 	{
-		results[,columnName1] = as.integer(team1)
-		results[,columnName2] = as.integer(team2)
+		results[,columnName1] = suppressWarnings(as.integer(team1)) # NA introduced by coercion
+		results[,columnName2] = suppressWarnings(as.integer(team2))
 	}
 	else
 	{
-		results[,columnName1] = as.factor(team1)
-		results[,columnName2] = as.factor(team2)
+		results[,columnName1] = team1
+		results[,columnName2] = team2
 	}
+
+  results[,columnName] = NULL
 	results
 }
